566af3dd16b75fec81b04a286f733e9d16697081
jerkey <jerkey@g-cipher.net>
2016-02-27 09:51:01 +0000

    would work if Stream::parseInt() weren't missing

--- vending1.ino
+++ vending1.ino
@@ -1,12 +1,30 @@
-byte pins [] = { 2,3,4,5,6,7,8,9,10,11,12,13,A0,A1,A2,A3,A4,A5 };
+#define NUMROWS 5
+#define NUMCOLS 10
+byte rows [NUMROWS] = { 13,12,11,10,9 }; // relays connecting to positive DC
+byte cols [NUMCOLS] = { 8,7,6,5,4,3,2,A2,A3,A4 }; // relays connecting to negative DC
+unsigned long inputValue = 0;
+#define PRIMEVAL 987234
 
 void setup() {
-    pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("vending1.ino");
+  for (int i=0; i < NUMROWS; i++) pinMode(rows[i], OUTPUT);
+  for (int i=0; i < NUMCOLS; i++) pinMode(cols[i], OUTPUT);
 }
 
 void loop() {
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    delay(1000);
+  if (Serial.available() > 0) {
+    inputValue = Serial.parseInt();
+    Serial.println(inputValue);
+    int vendNum = inputValue - PRIMEVAL;
+    if ((vendNum >= 0) && (vendNum < NUMROWS*NUMCOLS)) {
+      Serial.print("Dispensing product number ");
+      Serial.println(vendNum);
+      digitalWrite(rows[vendNum / NUMCOLS], HIGH);
+      digitalWrite(cols[vendNum % NUMCOLS], HIGH);
+      delay(1000);
+      digitalWrite(rows[vendNum / NUMCOLS], LOW);
+      digitalWrite(cols[vendNum % NUMCOLS], LOW);
+    }
+  }
 }
