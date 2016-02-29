7bae4cd6f0425681080cd00e3bf70c8b928780f8
jerkey <jerkey@g-cipher.net>
2016-02-29 11:25:25 +0000

    run the motor until interrupted or timeout

--- vending1.ino
+++ vending1.ino
@@ -3,7 +3,9 @@
 byte rows [NUMROWS] = { 13,12,11,10,9 }; // relays connecting to positive DC
 byte cols [NUMCOLS] = { 8,7,6,5,4,3,2,A2,A3,A4 }; // relays connecting to negative DC
 unsigned long inputValue = 0;
-#define PRIMEVAL 1000
+unsigned long startMotorTime = 0;
+#define PRIMEVAL 1000 // send this number plus the product number to dispense
+#define MOTORTIMEOUT 3000 // maximum time motor can run
 
 void setup() {
   Serial.begin(9600);
@@ -30,8 +32,16 @@ void loop() {
       Serial.print(cols[vendNum % NUMCOLS]);
       digitalWrite(rows[vendNum / NUMCOLS], HIGH);
       digitalWrite(cols[vendNum % NUMCOLS], HIGH);
-      delay(2000);
-      Serial.println(" and done.");
+      Serial.print(" ");
+      startMotorTime = millis(); // store the time we started the motor
+      byte progressBar = 0; // for displaying chars while motor turns
+      while ((Serial.available() == 0) && (millis() - startMotorTime < MOTORTIMEOUT)){
+        delay(100);
+        Serial.print(progressBar);
+        progressBar = (progressBar + 1) % 10; // count digits 01234567890123456...
+      }
+      while (Serial.available() > 0) Serial.read(); // empty the serial buffer
+      Serial.println("and done.");
       digitalWrite(rows[vendNum / NUMCOLS], LOW);
       digitalWrite(cols[vendNum % NUMCOLS], LOW);
     }
